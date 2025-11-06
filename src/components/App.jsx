4991647097988828775150f42240d1346499d254
fauxfox117 <SABolin1@me.com>
2025-11-06 17:21:39 -0500

    small update for my loves

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,12 @@
 import "./App.css";
 import Header from "./Header";
 import Main from "./Main";
-import Formula1 from "./Formula1";
 
 function App() {
   return (
     <>
       <Header />
       <Main />
-      <Formula1 selectedPhoto="enrico" />
     </>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
   return (
     <div className="header">
       <img className="header__logo" src={headerLogo} alt="App Logo" />
-      <h1 className="header__title">Hello Vite!</h1>
+      <h1 className="header__title">Hello Template!</h1>
     </div>
   );
 }
